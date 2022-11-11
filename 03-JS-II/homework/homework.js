a0b23603c5032f81c31a040081d7b40ff706c407
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-10 23:41:47 -0300

    Update 03-JS-II/homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,18 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  return Math.max (x, y);
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return 'Allowed';
+} else {
+    return 'Not allowed';
+}
 }
   
 function conection(status) {
@@ -19,6 +25,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1){
+    return 'Online';
+} else if (status === 2){
+    return 'Away';
+} else {
+    return 'Offline';
+}
 }
 
 function saludo(idioma) {
@@ -28,6 +41,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === 'aleman'){
+    return 'Guten Tag!';
+} else if (idioma === 'mandarin'){
+    return 'Ni Hao!';
+} else if (idioma === 'ingles'){
+    return 'Hello!';
+} else {
+    return 'Hola!';
+}
 }
 
 function colors(color) {
@@ -38,18 +60,40 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case 'blue':
+        console.log ('This is blue');
+        break;
+    case 'red':
+        console.log ('This is red');
+        break;
+    case 'green':
+        console.log ('This is green');
+        break;
+    case 'orange':
+        console.log ('This is orange');
+        break;
+    default:
+        console.log ('Color not found')
+}  
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5){
+    return true;
+} return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero > 20 && numero < 50){
+    return true;
+} return false;
 }
 
 function esEntero(numero) {
@@ -60,6 +104,9 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (Number.isInteger (numero)){
+    return true;
+} return false;  
 }
 
 function fizzBuzz(numero) {
@@ -67,6 +114,13 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0){
+    return 'FizzBuzz';
+} if (numero % 3 === 0 && numero % 5 !== 0){
+    return 'Fizz';
+} if (numero % 3 !== 0 && numero % 5 === 0){
+    return 'Buzz';
+}
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -76,6 +130,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (Math.sign (num1) === -1 || Math.sign (num2) === -1 || Math.sign (num3) === -1){
+    return 'Hay negativos';
+  } 
+  else if (num1 === 0 || num2 === 0 || num3 === 0){
+      return 'Error';
+  }
+  else if (num1 > num2 && num1 > num3 && Math.sign (num1) === 1){
+      return 'Número 1 es mayor y positivo';
+  }
+  else if (num3 > num1 && num3 > num2){
+    return (num3 + 1);
+  } 
+  else {
+    return false;
+}  
 }
 
 function esPrimo(numero) {
