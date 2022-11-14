54d23d69aaf34936e5cfdfedd5de61e35a33ccbc
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-14 14:53:07 -0300

    Update 03-JS-II/homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -120,7 +120,7 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3 === 0 && numero % 5 === 0) {
+  if (numero % 15 === 0) {
   return 'FizzBuzz';
   }
   if (numero % 3 === 0) {
