9dded3a729837c580cd63935c2e7397d6d971a62
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-15 18:51:37 -0300

    Update 04-JS-III/homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  const array = 0;
+  for (var i = 0; i < array.length; i++){
+    return array[i];
+  }
 }
 
 
