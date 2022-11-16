f5f3f8227c032b0190a3668d8fb579258cb5f714
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-16 17:52:39 -0300

    Update 04-JS-III/homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -59,7 +59,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  return palabras.concat ();
+  return palabras.join (' ');
 }
 
 
