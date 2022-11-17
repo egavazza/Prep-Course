35c697eed38fc10f9297605f317827f225adfed6
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-17 17:34:38 -0300

    Update 04-JS-III/homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -87,6 +87,11 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var suma = 0;
+  for (var i = 0; i < resultadosTest.length; i++){
+  suma += resultadosTest [i];
+}
+var promedio = suma / resultadosTest.length;
 }
 
 
