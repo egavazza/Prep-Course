3a5997f8e4474b4f4c85ea0ebf1de110ffa9c109
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-17 17:47:20 -0300

    Update 04-JS-III/homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -99,6 +99,7 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+return Math.max (numeros);
 }
 
 
