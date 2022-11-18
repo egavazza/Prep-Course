89a9b85e3622a8aa35fba599aa61402e0e204c72
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-17 21:27:40 -0300

    Update 04-JS-III/homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -161,8 +161,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  const isEqual = (arreglo) => arreglo === arreglo;
-  return (arreglo.every (isEqual))
+  for (var i = 0; i < arreglo.length -1; i++) {
+    if ( arreglo [i] !== arreglo[i + 1]){
+      return false;
+    }
+  }
+  return true;
 } 
 
 
