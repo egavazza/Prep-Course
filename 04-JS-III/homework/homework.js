9a4c1c73bf50ab7157062baff6f1fc197b3f5ab4
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-22 10:31:54 -0300

    Update 04-JS-III/homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -194,12 +194,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  let mayorACien = []
+  let mayorA100 = []
   for (var i = 0; i < array.lenght; i++) {
     if ((array [i] >100)){
-      mayorACien.push (array [i]);
+      mayorA100.push (array [i]);
     }
-    return mayorACien;
+    return mayorA100;
   }
 }
 
