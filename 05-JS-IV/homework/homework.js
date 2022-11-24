c98e1e884ee757ec04e8d295ab53272e261091d4
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-24 20:44:41 -0300

    Update 05-JS-IV/homework

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -65,6 +65,11 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if (usuario.email !== 'null'){
+    return 'true';
+  } else {
+    return 'false';
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
