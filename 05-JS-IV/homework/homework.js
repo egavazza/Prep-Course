1d2594dc7e0d26437b88d7bee4752311a1cf91fa
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-27 19:43:02 -0300

    Update 05-JS-IV/homework

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -98,6 +98,8 @@ function actualizarPassword (usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo (usuario, nuevoAmigo) {
