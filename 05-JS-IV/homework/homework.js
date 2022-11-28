44d9ecf1ef5bb525dff84f85ae83d6830ed93161
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-27 23:37:40 -0300

    Update 05-JS-IV/homework

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -131,6 +131,11 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0;
+  for (var i = 0; i < usuario.posts.length; i ++) {
+    suma = suma + usuario.posts[i].likes;
+  }
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento (producto) {
