f1903e8cf6f0c7e1be46a28edbbe62def296c6ba
Ezequiel Gavazza <egavazza@gmail.com>
2022-11-27 23:57:01 -0300

    Update 05-JS-IV/homework

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -148,7 +148,10 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto [calcularPrecioDescuento] = function () {
+    console.log (producto.precio - (producto.precio * producto.porcentajeDeDescuento));
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
